e611bb112ddc211c8e7cc3587423d7f152aefd2c
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 19:53:55 +0300

    Add files via upload

---
+++ PascalTriangle/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int[,] arr = new int[num + 1, num + 1];
+            for (int row = 1; row < num + 1; row++)
+            {
+                for (int col = 1; col < row + 1; col++)
+                {
+                    if (row == 1 || col == row)
+                    {
+                        arr[row, col] = 1;
+                    }
+                    else
+                    {
+                        arr[row, col] = arr[row - 1, col] + arr[row - 1, col - 1];
+                    }
+                    Console.Write(arr[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
