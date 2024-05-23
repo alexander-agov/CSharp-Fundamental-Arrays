a44b57a65e044a4669c0ddd85104501231c0223b
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 19:55:11 +0300

    Add files via upload

---
+++ RecursiveFibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Reflection.Metadata.Ecma335;
+
+namespace RecursiveFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int result = GetFibonacci(n);
+            Console.WriteLine($"{result}");
+
+            static int GetFibonacci(int n)
+            {
+                if (n == 1 || n == 2)
+                    return 1;
+
+                return GetFibonacci(n - 1) + GetFibonacci(n - 2); //recursive calls
+            }
+        }
+    }
+}
