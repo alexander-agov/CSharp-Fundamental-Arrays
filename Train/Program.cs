41b6757ee6bb27894a1b3733486059558b1bfd43
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 20:01:20 +0300

    Add files via upload

---
+++ Train/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Markup;
+using System.Linq;
+namespace Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int[] wagon = new int[num];
+            int sum = 0;
+            for (int i = 0; i < num; i++)
+            {
+                int passengers = int.Parse(Console.ReadLine());
+                wagon[i] = passengers;
+                sum += wagon[i];
+            }
+            Console.WriteLine(string.Join(" ", wagon));
+            Console.WriteLine(sum);
+        }
+    }
+}
+
