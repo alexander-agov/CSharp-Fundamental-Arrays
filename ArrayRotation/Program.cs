f40784848b5de19cb737375fe17186013115f28a
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 19:25:35 +0300

    Add files via upload

---
+++ ArrayRotation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+namespace ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            int rotations = int.Parse(Console.ReadLine());
+            for (int i = 0; i < rotations; i++)
+            {
+                string firstElement = arr[0];
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    arr[j] = arr[j + 1];
+
+                }
+                arr[arr.Length - 1] = firstElement;
+            }
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
