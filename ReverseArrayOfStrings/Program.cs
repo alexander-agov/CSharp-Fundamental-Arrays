eb3025433074e4e47c93567c65d56f4ab21cff1e
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 19:56:09 +0300

    Add files via upload

---
+++ ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Markup;
+using System.Linq;
+
+namespace ReverseArrayOfStrings
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] values = Console.ReadLine().Split(" ");
+            for (int i = 0; i < values.Length / 2; i++)
+            {
+                string firstElement = values[i];
+                string lastElement = values[values.Length - 1 - i];
+                values[i] = lastElement;
+                values[values.Length - 1 - i] = firstElement;
+            }
+
+            //Array.Reverse(values);
+
+            //List<string> list = new List<string>();
+            //for (int i = values.Length - 1; i > 0 ;i--) 
+            //{
+            //    list.Add(values[i]);
+            //}
+            //Console.WriteLine(string.Join(" ", list));
+
+            //for (int i = 0; i < values.Length; i++) { }
+            // Console.WriteLine(values[^(i + 1)]); //with ^ return the last element;
+                                                                                // always begin from the given number without 0, 0 exception!
+
+            Console.WriteLine(string.Join(" ", values));
+        }
+    }
+}
