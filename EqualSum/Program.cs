1f64ccb1d60bebdeb320878b0f532b3ac9a5b6fb
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 19:47:48 +0300

    Add files via upload

---
+++ EqualSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+namespace EqualSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int index = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    leftSum += nums[j];
+                }
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    rightSum += nums[j];
+                }
+
+                if (rightSum == leftSum)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index != -1)
+            {
+                Console.WriteLine($"{index}");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
