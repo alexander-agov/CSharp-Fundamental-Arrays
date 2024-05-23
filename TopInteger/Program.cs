b4898275f41a360cf5f6ab9070d7098b3674b541
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 19:58:12 +0300

    Add files via upload

---
+++ TopInteger/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+namespace TopInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split();
+            int[] newStr = new int[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                newStr[i] = int.Parse(str[i]);
+            }
+            for (int i = 0; i < newStr.Length; i++)
+            {
+                bool isTop = true;
+                for (int j = i + 1; j < newStr.Length; j++)
+                {
+                    if (newStr[i] <= newStr[j])
+                    {
+                        isTop = false;
+                        break;
+                    }
+                }
+                if (isTop)
+                {
+                    Console.Write($"{newStr[i]} ");
+                }
+            }
+        }
+    }
+}
+
