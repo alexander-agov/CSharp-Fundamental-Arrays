b7469dfaf6e8d8123594dd5a7f6358b8ab30d8aa
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 19:57:26 +0300

    Add files via upload

---
+++ SumEvenNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Markup;
+using System.Linq;
+namespace SumEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumEven = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sumEven += numbers[i];
+                }
+            }
+            Console.WriteLine(sumEven);
+        }
+    }
+}
+
